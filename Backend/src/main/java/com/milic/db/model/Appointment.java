5eda163442063f19b5fe67cc2818659b63c1999e
Dusan Milic <dusan@realigned.co>
2019-10-05 12:44:31 +0200

    fix

--- Backend/src/main/java/com/milic/db/model/Appointment.java
+++ Backend/src/main/java/com/milic/db/model/Appointment.java
@@ -2,6 +2,7 @@ package com.milic.db.model;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -9,7 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "appointments")
 public class Appointment {
 
   @Id
